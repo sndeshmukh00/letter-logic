c6108cb28ea765427151025da5961a79ad918d32
sndeshmukh00 <shanu05.official@gmail.com>
2024-06-13 20:00:36 +0530

    board created

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View, ScrollView } from "react-native";
 import { COLORS } from "./src/constants";
 import SafeViewAndroid from "./src/safeViewAndroid";
 import Keyboard from "./src/components/Keyboard";
 
 export default function App() {
+  const word = "hello";
+  const letters = word.split("");
+  const complexity = 1; // 1 - easy, 2 - medium, 3 - hard
+  // const rows = new Array(complexity).fill(null);
+  const rows = new Array(Math.ceil(letters.length / complexity)).fill(
+    new Array(letters.length).fill("a")
+  );
+
+  const onKeyPressed = (key) => {
+    console.warn(key);
+  };
   return (
     <SafeAreaView style={[SafeViewAndroid.AndroidSafeArea, styles.container]}>
       <StatusBar style="light" />
 
       <Text style={styles.title}>Letter Logic</Text>
 
-      <Keyboard />
+      <ScrollView style={styles.board}>
+        {rows.map((row) => (
+          <View style={styles.row}>
+            {row.map((letter) => (
+              <View style={styles.cell}>
+                <Text style={styles.cellText}>{letter.toUpperCase()}</Text>
+              </View>
+            ))}
+          </View>
+        ))}
+      </ScrollView>
+
+      <Keyboard onKeyPressed={(key) => onKeyPressed(key)} />
     </SafeAreaView>
   );
 }
@@ -28,5 +51,31 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontWeight: "bold",
     letterSpacing: 5,
+  },
+  board: {
+    alignSelf: "stretch",
+    height: 100,
+    marginVertical: 20,
+  },
+  row: {
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifyContent: "center",
+  },
+  cell: {
+    borderColor: COLORS.darkgrey,
+    flex: 1,
+    aspectRatio: 1,
+    borderWidth: 2,
+    margin: 3,
+    maxWidth: 70,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cellText: {
+    color: COLORS.lightgrey,
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
