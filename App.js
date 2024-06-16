38e58c3e94e03bd5e94d04f831e18aae9eceb4f4
sndeshmukh00 <shanu05.official@gmail.com>
2024-06-16 15:55:23 +0530

    Removing Unused code

--- App.js
+++ App.js
@@ -22,36 +22,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
     // justifyContent: "center",
   },
-  title: {
-    color: COLORS.lightgrey,
-    fontSize: 32,
-    fontWeight: "bold",
-    letterSpacing: 5,
-  },
-  board: {
-    alignSelf: "stretch",
-    height: 100,
-    marginVertical: 20,
-  },
-  row: {
-    flexDirection: "row",
-    alignSelf: "stretch",
-    justifyContent: "center",
-  },
-  cell: {
-    borderColor: COLORS.darkgrey,
-    flex: 1,
-    aspectRatio: 1,
-    borderWidth: 2,
-    margin: 3,
-    maxWidth: 70,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  cellText: {
-    color: COLORS.lightgrey,
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
 });
