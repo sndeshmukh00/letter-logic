0a0bbbd86d1c03c05b23ba047463cb98db385011
sndeshmukh00 <shanu05.official@gmail.com>
2024-06-18 21:11:53 +0530

    Home Screen Setting Popup added

---
+++ src/components/Popups/SettingPopup.js
@@ -0,0 +1,197 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Modal,
+  BackHandler,
+  TouchableWithoutFeedback,
+} from "react-native";
+import {
+  MaterialIcons,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
+import ConfirmationPopup from "./ConfirmationPopup";
+
+const SettingMenu = ({
+  visible,
+  onClose,
+  onToggleMusic,
+  onToggleSound,
+  onToggleVibration,
+  musicOn,
+  soundOn,
+  vibrationOn,
+  isLoggedIn,
+}) => {
+  const [isDeleteConfirmationVisible, setisDeleteConfirmationVisible] =
+    useState(false);
+  const [isQuitConfirmationVisible, setIsQuitConfirmationVisible] =
+    useState(false);
+
+  const onQuit = () => {
+    BackHandler.exitApp();
+  };
+  const handleLogout = () => {
+    setIsQuitConfirmationVisible(false);
+    onClose();
+  };
+
+  const handleDeleteMyData = () => {
+    setisDeleteConfirmationVisible(true);
+  };
+  const handleConfirmDelete = () => {
+    setisDeleteConfirmationVisible(false);
+  };
+
+  const handleCancelDelete = () => {
+    setisDeleteConfirmationVisible(false);
+  };
+
+  const quitHandler = () => {
+    setIsQuitConfirmationVisible(true);
+  };
+  const handleConfirmQuit = () => {
+    onQuit();
+  };
+
+  const handleCancelQuit = () => {
+    setIsQuitConfirmationVisible(false);
+  };
+  return (
+    <Modal visible={visible} transparent={true} animationType="slide">
+      <TouchableWithoutFeedback onPress={onClose}>
+        <View style={styles.overlay}>
+          <TouchableWithoutFeedback onPress={() => {}}>
+            <View style={styles.popup}>
+              <View style={styles.banner}>
+                <Text style={styles.bannerText}>Settings</Text>
+              </View>
+              <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={styles.button} onPress={onClose}>
+                  <Text style={styles.buttonText}>
+                    {isLoggedIn ? "Logout" : "Login"}
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={handleDeleteMyData}
+                >
+                  <Text style={styles.buttonText}>Delete my data</Text>
+                </TouchableOpacity>
+                {isDeleteConfirmationVisible && (
+                  <ConfirmationPopup
+                    title="Confirm Deletion"
+                    message={`Are you sure you want to delete your data?
+                \nThis action will remove all your game progress & statistics.
+                \nYou can't undo it.`}
+                    visible={isDeleteConfirmationVisible}
+                    onConfirm={handleConfirmDelete}
+                    onCancel={handleCancelDelete}
+                  />
+                )}
+                <TouchableOpacity style={styles.button} onPress={quitHandler}>
+                  <Text style={styles.buttonText}>Quit</Text>
+                </TouchableOpacity>
+                {isQuitConfirmationVisible && (
+                  <ConfirmationPopup
+                    title="Confirm Quitting"
+                    message={`Are you sure you want to quit?`}
+                    visible={isQuitConfirmationVisible}
+                    onConfirm={handleConfirmQuit}
+                    onCancel={handleCancelQuit}
+                  />
+                )}
+              </View>
+              <View style={styles.iconsContainer}>
+                <TouchableOpacity onPress={onToggleMusic}>
+                  <MaterialIcons
+                    name={musicOn ? "music-note" : "music-off"}
+                    size={32}
+                    color="white"
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onToggleSound}>
+                  <MaterialIcons
+                    name={soundOn ? "volume-up" : "volume-off"}
+                    size={32}
+                    color="white"
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={onToggleVibration}>
+                  <MaterialCommunityIcons
+                    name={vibrationOn ? "vibrate" : "vibrate-off"}
+                    size={32}
+                    color="white"
+                  />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <Ionicons
+                    name="help-circle-outline"
+                    size={32}
+                    color="white"
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </View>
+      </TouchableWithoutFeedback>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  popup: {
+    width: "80%",
+    backgroundColor: "#333",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+  },
+  banner: {
+    position: "absolute",
+    top: -25,
+    backgroundColor: "#ff6347",
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  bannerText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  buttonsContainer: {
+    marginTop: 40,
+    width: "100%",
+  },
+  button: {
+    backgroundColor: "#ff6347",
+    paddingVertical: 10,
+    borderRadius: 5,
+    marginVertical: 5,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  iconsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: "100%",
+  },
+});
+
+export default SettingMenu;
