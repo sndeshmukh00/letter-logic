63b0319787a94e5f0bfaf3e4bf9352329268fda2
sndeshmukh00 <shanu05.official@gmail.com>
2024-07-05 01:06:53 +0530

    Implementing redux store

--- src/Navigation/AppNavigator.js
+++ src/Navigation/AppNavigator.js
@@ -1,15 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import AppStack from "./AppStack";
 import AuthStack from "./AuthStack";
 import { AuthContext, AuthProvider } from "./AuthContext";
+import { useDispatch, useSelector } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { setUserData } from "../store/actions/setUserData";
+import { getUserInfo } from "../api/getUserInfo";
 
 const AppNavigator = () => {
   const Stack = createStackNavigator();
-
   const { isLoggedIn } = useContext(AuthContext);
+  const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        if (isLoggedIn) {
+          const token = await AsyncStorage.getItem("@token");
+          const userData = await AsyncStorage.getItem("userData");
+
+          const userDataResponse = await getUserInfo(userData.email, token);
+          if (userDataResponse && userDataResponse.success) {
+            await AsyncStorage.setItem(
+              "userData",
+              JSON.stringify(userDataResponse.user)
+            );
+
+            dispatch(setUserData(updatedUserData));
+          } else return { message: "Something went wrong!" };
+        } else {
+          // Fetch data from AsyncStorage if not logged in
+          const userData = await AsyncStorage.getItem("userData");
+          if (userData) {
+            dispatch(setUserData(JSON.parse(userData)));
+          }
+        }
+      } catch (error) {
+        console.error("Failed to fetch user data", error);
+      }
+    };
+
+    fetchUserData();
+  }, [isLoggedIn, dispatch]);
 
   return (
     <NavigationContainer>
